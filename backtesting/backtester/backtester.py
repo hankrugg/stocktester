8798b4a9117fcec504a86e558bd94e3b70329997
hankrugg <hankrugg2@gmail.com>
2025-02-25 21:04:05 -1000

    started data_loaded and backtester

---
+++ backtesting/backtester/backtester.py
@@ -0,0 +1,8 @@
+"""
+This is the class where all the work will be done.
+"""
+
+
+class Backtester:
+    def __init__(self):
+        pass
---
+++ backtesting/data/data_loader.py
@@ -0,0 +1,10 @@
+"""
+This class is responsible for loading and validating data that the user provides.
+This class is designed to strictly prohibit any form of data leakage so the user can fully trust
+the results of their analysis.
+"""
+
+
+class DataLoader:
+    def __init__(self):
+        pass
