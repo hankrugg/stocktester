6cbc0111a2af88e0cc0073161ed3b99fffc897ca
hankrugg <hankrugg2@gmail.com>
2025-04-28 20:57:29 -0600

    start excecution

---
+++ backtesting/excecution/broker.py
@@ -0,0 +1,14 @@
+"""
+Created on April 28 by Hank Rugg
+"""
+
+class Broker:
+    
+    def __init__(self):
+        pass
+
+    def validate_order(order):
+        pass
+
+    def excecute_order(order):
+        pass
